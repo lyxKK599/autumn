f8e7858c4ef56d46c6f681fca2ca9881c3adf041
LYX <lyx599a2021@163.com>
2021-08-28 21:56:37 +0800

    11

--- app/src/main/java/com/example/autumn/MainActivity.java
+++ app/src/main/java/com/example/autumn/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         initView();
         for(int i=0 ; i<mTitles.length ; i++)
         {
